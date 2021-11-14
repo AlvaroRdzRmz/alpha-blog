5a55425009702b428978490f0709bc20999f1492
Alvaro Rodriguez <ALVARO.RAMIREZ1@ibm.com>
2021-11-14 15:25:07 -0600

    Add Show Model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'statics#home'
   get 'about', to: 'statics#about'
+  resources :articles, only: [:show]
+  
 end
